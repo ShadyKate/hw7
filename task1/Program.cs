b33084ccf59e8b9e58140f32c623faf4fb4ad773
Kate <katehappy16@gmail.com>
2023-12-12 00:10:56 +0300

    задача 1

---
+++ task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+ //Использовать рекурсию, не использовать циклы.
+Console.Clear();
+string printNumbersIn(int mNumber, int nNumber)
+{
+    if(mNumber < nNumber)
+    {
+        if (mNumber == nNumber)
+        {
+            return $"{nNumber} ";
+        }
+        return printNumbersIn(mNumber, nNumber - 1) + $"{nNumber} "; 
+    }
+    else{
+
+        if (mNumber == nNumber)
+            {
+                return $"{mNumber} ";
+            }
+        return printNumbersIn(mNumber - 1, nNumber) + $"{mNumber} "; 
+    }
+}
+
+Console.WriteLine("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число(постарайтесь сделать так, чтобы второе число было больше): ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+if(firstNumber > secondNumber)
+{
+    Console.WriteLine("Эх, ладно придётся дописать код");
+}
+Console.WriteLine($"числа лежащие между введёнными Вами числами {printNumbersIn(firstNumber, secondNumber)}");
+
